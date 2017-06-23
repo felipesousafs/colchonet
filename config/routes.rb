df0b4be86f468339c326dab2fa8528cac8c4fb8d
Felipe Sousa <felipe.sousa.fs@gmail.com>
2017-06-23 09:26:25 -0300

    Capítulo 9. O cadastro do usuário e a confirmação da identidade - Page 191

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   resources :rooms
   resources :users
 
+  resource :confirmation, :only => [:show]
+
   root :to => "home#index"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170621120552) do
+ActiveRecord::Schema.define(version: 20170623120629) do
 
   create_table "rooms", force: :cascade do |t|
     t.string   "title"
@@ -26,8 +26,10 @@ ActiveRecord::Schema.define(version: 20170621120552) do
     t.string   "password_digest"
     t.string   "location"
     t.text     "bio"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.datetime "confirmed_at"
+    t.string   "confirmation_token"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
